df09c7f1a8486cb484d6f0e51cc43a127307624c
mansan1230 <84826972+mansan1230@users.noreply.github.com>
2025-12-15 23:31:09 +0800

    Delete Emily Tang Portfolio directory

--- Emily Tang Portfolio/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/portfolio/Emily Tang Portfolio', // 這裡填你的 Repo 名字，前後要有斜線
-})
