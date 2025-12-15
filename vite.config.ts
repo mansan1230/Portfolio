a1f4144d98d95380504ae4d05b9b458c6948d2a3
mansan1230 <84826972+mansan1230@users.noreply.github.com>
2025-12-16 00:46:30 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // 這裡填你的 Repo 名字，前後要有斜線
+  base: '/Portfolio/', // Correct base path for https://mansan1230.github.io/Portfolio/
 })
